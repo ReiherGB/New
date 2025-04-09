a6fca7a3e195b6abaf13c6e7d3396752fc69ed7d
ReiherGB <fr.pachecor@duocuc.cl>
2025-04-09 09:44:44 -0400

    Add files via upload

---
+++ Trabajo 2/clase.py
@@ -0,0 +1,50 @@
+# Suma
+
+S01 = int(input("Dime el primer numero para la sumatoria: "))
+S02 = float(input("Dime el segundo numero de tu sumatoria: "))
+
+
+# Operacion 
+
+R1 = S01 + S02 
+
+print ( "El resultado es ", R1)
+
+
+# Resta 
+
+R01 = int(input("Dime el primer numero patra la resta: "))
+R02 = float(input("Dime el segundo numero para tu resta: "))
+
+# Operacion 
+
+R2 = R01 - R02 
+
+print ( "El resultado es ", R2)
+
+
+# Multiplicacion
+
+M01 = int(input("Dime el primer numero para la multiplicacion: "))
+M02 = float(input("Dime El segundo numero para tu multiplicacion: "))
+
+# Operaion
+
+R3 = M01 * M02 
+
+print ( "El resultado es ", R3)
+
+
+# Division
+
+D01 = int (input("Dime el primer numero para la division: "))
+D02 = float(input("Dime el segundo numero para tu division: "))
+
+# Operacion 
+
+R4 = D01 / D02 
+
+print ( "El resultado es ", R4)
+
+
+print ("!!Enhorabuena¡¡, has terminado todas tus operaciones")
